f8c3dd7f0a3a091327174a41c0a1185f8233288f
bartek <Bartek.NNAAN@wp.pl>
2019-02-22 15:29:56 +0100

    call api

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || '9def350f3b5100fb7c547cc2c231ba6f989af9479d243e9fe55b4ee3f78106ad',
+    SECRET: process.env.SECRET || 'f8106d6aec7db9123575a3d5fcf1ab4e5237973cacba835cb7210b9d1dabe749',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
